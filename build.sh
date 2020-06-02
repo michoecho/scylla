6596b00c9ebced9fc71a3e2e6feda82ceb638d3b
Michał Chojnowski <mc394134@students.mimuw.edu.pl>
2020-06-03 00:44:20 +0200

    Use `set -e` instead of many && in the temporary build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-# Install gzip, snappy and thrift >= 0.11.0
+# Install gzip, snappy and thrift >= 0.11.0 first
 
 SCYLLA="$PWD"
 P4S="$PWD/parquet4seastar"
@@ -9,20 +10,20 @@ P4S_LIB="$P4S/build/libparquet4seastar.a"
 
 if [ ! -d parquet4seastar ]; then
 	git clone git@github.com:michoecho/parquet4seastar "$P4S"
-fi &&
+fi
 
-git submodule update --init &&
-./configure.py --mode dev &&
-ninja -C build/dev/seastar libseastar.a &&
+git submodule update --init
+./configure.py --mode dev
+ninja -C build/dev/seastar libseastar.a
 
-cd "$P4S" &&
-git checkout dev &&
-mkdir -p build &&
-cd build &&
-cmake "-DCMAKE_PREFIX_PATH=$SCYLLA/build/dev/seastar" "-DCMAKE_MODULE_PATH=$SCYLLA/seastar/cmake" .. &&
-make parquet4seastar apps -j4 &&
+cd "$P4S"
+git checkout dev
+mkdir -p build
+cd build
+cmake "-DCMAKE_PREFIX_PATH=$SCYLLA/build/dev/seastar" "-DCMAKE_MODULE_PATH=$SCYLLA/seastar/cmake" ..
+make parquet4seastar apps -j4
 
-cd "$SCYLLA" &&
+cd "$SCYLLA"
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
@@ -30,8 +31,8 @@ cd "$SCYLLA" &&
 # OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-VIMARGS=(-N -u NONE -i NONE -n -c "set nomore") &&
-vim $VIMARGS "+norm /rule cxxj0/-Ii-I$P4S_INCLUDE :x" build.ninja &&
-vim $VIMARGS "+norm /seastar_libsf=lli$P4S_LIB :x" build.ninja &&
+VIMARGS=(-N -u NONE -i NONE -n -c "set nomore")
+vim $VIMARGS "+norm /rule cxxj0/-Ii-I$P4S_INCLUDE :x" build.ninja
+vim $VIMARGS "+norm /seastar_libsf=lli$P4S_LIB :x" build.ninja
 
 ninja build/dev/scylla
