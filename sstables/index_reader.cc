6a15ebb76569ee0e1e88972ffd5f0862d7d4e256
Michał Chojnowski <michocho98@gmail.com>
2024-11-04 20:46:24 +0100

    sstables/mx/reader: for supported key types, use the BTI index instead of the old index

--- sstables/index_reader.cc
+++ sstables/index_reader.cc
@@ -1,5 +1,6 @@
 #include "index_reader.hh"
 #include "downsampling.hh"
+#include "sstables/trie/trie.hh"
 
 namespace sstables {
 
@@ -1382,8 +1383,21 @@ make_index_reader(
     reader_permit permit,
     tracing::trace_state_ptr trace_state,
     use_caching caching,
-    bool single_partition_read
+    bool single_partition_read,
+    allow_trie trie_allow
 ) {
+    if (trie_allow && sst->has_component(component_type::Partitions) && !sst->get_schema()->ks_name().contains("system")) {
+        sstlog.debug("Using trie index for table {}.{}", sst->get_schema()->ks_name(), sst->get_schema()->cf_name());
+        return trie::make_bti_index_reader(
+            *sst->_partition_index_file_cached,
+            *sst->_row_index_file_cached,
+            sst->trie_root_offset(),
+            sst->data_size(),
+            sst->get_schema(),
+            std::move(permit)
+        );
+    }
+    sstlog.debug("Using normal index for table {}.{}", sst->get_schema()->ks_name(), sst->get_schema()->cf_name());
     return std::make_unique<index_reader_old>(
         std::move(sst),
         std::move(permit),
