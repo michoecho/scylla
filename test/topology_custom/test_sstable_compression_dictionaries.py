41d514240843788d3dbac9d0c8bbee5d3e844984
Michał Chojnowski <michocho98@gmail.com>
2025-03-11 11:33:19 +0100

    memlimit test

--- test/topology_custom/test_sstable_compression_dictionaries.py
+++ test/topology_custom/test_sstable_compression_dictionaries.py
@@ -10,8 +10,14 @@ import itertools
 from test.pylib.manager_client import ManagerClient, ServerInfo
 from test.pylib.rest_client import read_barrier
 from cassandra.cluster import ConsistencyLevel
+from test.pylib.rest_client import ScyllaMetrics
 
 logger = logging.getLogger(__name__)
+
+async def get_metrics(manager: ManagerClient, servers: list[ServerInfo]) -> list[ScyllaMetrics]:
+    return await asyncio.gather(*[manager.metrics.query(s.ip_addr) for s in servers])
+def dict_memory(metrics: list[ScyllaMetrics]) -> int:
+    return sum([m.get("scylla_sstable_compression_dicts_total_live_memory_bytes") for m in metrics])
 
 async def test_retrain_dict(manager: ManagerClient):
     """
@@ -143,3 +149,100 @@ async def test_retrain_dict(manager: ManagerClient):
     assert (await cql.run_async("SELECT COUNT(name) FROM system.dicts"))[0][0] == 0
 
     logger.info("Test completed successfully")
+
+#@pytest.mark.skip("slow")
+async def test_dict_memory_limit(manager: ManagerClient):
+    # Bootstrap cluster and configure server
+    logger.info("Bootstrapping cluster")
+    servers = (await manager.servers_add(1, cmdline=[
+        '--logger-log-level=storage_service=debug',
+        '--logger-log-level=api=trace',
+        '--logger-log-level=database=debug',
+        '--abort-on-seastar-bad-alloc',
+        #'--memory=1G', # test.py forces --memory=1G, and it can't be overwritten
+        '--smp=2',
+        '--sstable-compression-with-dictionaries-memory-budget-fraction=0.005',
+        '--dump-memory-diagnostics-on-alloc-failure-kind=all',
+    ]))
+
+    intended_dict_memory_bugdet = 0.005 * (1*1024*1024*1024 / 2)
+
+    # Create keyspace and table
+    cql = manager.get_cql()
+    await cql.run_async(
+        "CREATE KEYSPACE test WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}"
+    )
+
+    for algo in ['ZstdWithDictsCompressor', 'LZ4WithDictsCompressor']:
+        logger.info(f"Creating table with algo {algo}")
+
+        await cql.run_async("CREATE TABLE test.test (pk int PRIMARY KEY, c blob);")
+
+        # Disable autocompaction
+        logger.info("Disabling autocompaction for the table")
+        await asyncio.gather(*[manager.api.disable_autocompaction(s.ip_addr, "test", "test") for s in servers])
+        blob = random.randbytes(2048)
+
+        async def get_data_size_for_server(server: ServerInfo) -> int:
+            sstable_info = await manager.api.get_sstable_info(server.ip_addr, "test", "test")
+            sizes = [x['data_size'] for s in sstable_info for x in s['sstables']]
+            return sum(sizes)
+
+        async def get_total_data_size() -> int:
+            return sum(await asyncio.gather(*[get_data_size_for_server(s) for s in servers]))
+
+        # Alter compression to zstd
+        logger.info("Altering table to use zstd compression")
+        await cql.run_async(
+            f"ALTER TABLE test.test WITH COMPRESSION = {{'sstable_compression': '{algo}'}};"
+        )
+
+        leeway = 500e3
+
+        for i in range(100):
+            lastblob = random.randbytes(2048) + blob
+            insert = cql.prepare("INSERT INTO test.test (pk, c) VALUES (?, ?);")
+            await cql.run_async(insert, [0, lastblob])
+            await asyncio.gather(*[manager.api.keyspace_flush(s.ip_addr, "test", "test") for s in servers])
+            await manager.api.retrain_dict(servers[0].ip_addr, "test", "test")
+            dict_mem = dict_memory(await get_metrics(manager, servers))
+            assert dict_mem < intended_dict_memory_bugdet + leeway
+            logger.info(f"Round 0, step {i}: size={(await get_total_data_size())}, dictmem={dict_mem}")
+
+        assert dict_mem > intended_dict_memory_bugdet - leeway
+        await asyncio.gather(*[manager.api.keyspace_upgrade_sstables(s.ip_addr, "test") for s in servers])
+        assert dict_memory(await get_metrics(manager, servers)) < leeway
+
+        for i in range(100):
+            lastblob = random.randbytes(2048) + blob
+            insert = cql.prepare("INSERT INTO test.test (pk, c) VALUES (?, ?);")
+            await cql.run_async(insert, [0, lastblob])
+            await asyncio.gather(*[manager.api.keyspace_flush(s.ip_addr, "test", "test") for s in servers])
+            await manager.api.retrain_dict(servers[0].ip_addr, "test", "test")
+            dict_mem = dict_memory(await get_metrics(manager, servers))
+            assert dict_mem < intended_dict_memory_bugdet + leeway
+            logger.info(f"Round 1, step {i}: size={(await get_total_data_size())}, dictmem={dict_mem}")
+
+        assert dict_mem > intended_dict_memory_bugdet - leeway
+
+        logger.info("Validating query results")
+        await manager.driver_connect(server=servers[0])
+        cql = manager.get_cql()
+        select = cql.prepare("SELECT c FROM test.test WHERE pk = ?;")
+        select.consistency_level = ConsistencyLevel.ALL;
+        results = await cql.run_async(select, [0])
+        assert results[0][0] == lastblob
+
+        logger.info("dicts before DROP")
+        results = await cql.run_async("SELECT * from system.dicts", [])
+        for r in results:
+            logger.info(r[0])
+
+        await cql.run_async("DROP TABLE test.test")
+
+        results = await cql.run_async("SELECT * from system.dicts", [])
+        logger.info("dicts after DROP")
+        for r in results:
+            logger.info(r[0])
+        await asyncio.gather(*[read_barrier(manager.api, s.ip_addr) for s in servers])
+        assert dict_memory(await get_metrics(manager, servers)) == 0
