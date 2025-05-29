5ba278cd079abef903b9c84357e1acf568e69b96
Michał Chojnowski <michal.chojnowski@scylladb.com>
2025-05-29 16:58:28 +0200

    format wip

--- sstables/sstable_version.cc
+++ sstables/sstable_version.cc
@@ -9,6 +9,7 @@
 #include "sstable_version.hh"
 #include "sstable_version_k_l.hh"
 #include "sstable_version_m.hh"
+#include "sstable_version_d.hh"
 
 namespace sstables {
 
@@ -17,9 +18,6 @@ const sstring sstable_version_constants::TEMPORARY_TOC_SUFFIX = "TOC.txt.tmp";
 
 sstable_version_constants::component_map_t sstable_version_constants::create_component_map() {
     return {
-        { component_type::Index, "Index.db"},
-        { component_type::Partitions, "TrieIndex.db"},
-        { component_type::Rows, "Rows.db"},
         { component_type::CompressionInfo, "CompressionInfo.db" },
         { component_type::Data, "Data.db" },
         { component_type::TOC, TOC_SUFFIX },
@@ -43,6 +41,8 @@ sstable_version_constants::get_component_map(sstable_version_types version) {
         case sstable_version_types::md:
         case sstable_version_types::me:
             return sstable_version_constants_m::_component_map;
+        case sstable_version_types::da:
+            return sstable_version_constants_d::_component_map;
     }
     // Should never reach this.
     // Compiler should complain if the switch above does no cover all sstable_version_types values.
@@ -52,6 +52,7 @@ sstable_version_constants::get_component_map(sstable_version_types version) {
 const sstable_version_constants::component_map_t sstable_version_constants_k_l::create_component_map() {
     auto result = sstable_version_constants::create_component_map();
     result.emplace(component_type::Digest, "Digest.sha1");
+    result.emplace(component_type::Index, "Index.db");
     return result;
 }
 
@@ -61,10 +62,23 @@ const sstable_version_constants::component_map_t sstable_version_constants_k_l::
 const sstable_version_constants::component_map_t sstable_version_constants_m::create_component_map() {
     auto result = sstable_version_constants::create_component_map();
     result.emplace(component_type::Digest, "Digest.crc32");
+    result.emplace(component_type::Index, "Index.db");
+    return result;
+}
+
+const sstable_version_constants::component_map_t sstable_version_constants_d::create_component_map() {
+    auto result = sstable_version_constants::create_component_map();
+    result.emplace(component_type::Digest, "Digest.crc32");
+    result.emplace(component_type::Partitions, "Partitions.db");
+    result.emplace(component_type::Rows, "Rows.db");
+    result.emplace(component_type::Index, "Index.db");
     return result;
 }
 
 const sstable_version_constants::component_map_t sstable_version_constants_m::_component_map =
         sstable_version_constants_m::create_component_map();
+
+const sstable_version_constants::component_map_t sstable_version_constants_d::_component_map =
+        sstable_version_constants_d::create_component_map();
 
 }
