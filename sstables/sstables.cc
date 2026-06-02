fc75c05ee9d93a5552fda6aef384de958f77adb9
Michał Chojnowski <michal.chojnowski@scylladb.com>
2026-06-02 15:33:47 +0200

    sstables: introduce sstable format version `mu`
    As of this patch, the format is identical to `mt` (and `ms`).
    In a later patch we may modify it so that it differs from `mt`.
    
    As of this patch, the version is usable only to unit tests.
    It will be made selectable to the config in a later patch.

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -260,6 +260,7 @@ const std::unordered_map<sstable_version_types, sstring, enum_hash<sstable_versi
     { sstable_version_types::me , "me" },
     { sstable_version_types::ms , "ms" },
     { sstable_version_types::mt , "mt" },
+    { sstable_version_types::mu , "mu" },
 };
 
 const std::unordered_map<sstable_format_types, sstring, enum_hash<sstable_format_types>> format_string = {
@@ -2803,6 +2804,7 @@ sstring sstable::component_basename(const sstring& ks, const sstring& cf, versio
     case sstable::version_types::me:
     case sstable::version_types::ms:
     case sstable::version_types::mt:
+    case sstable::version_types::mu:
         return v + "-" + g + "-" + f + "-" + component;
     }
     on_internal_error(sstlog, seastar::format("invalid version {} for sstable: table={}.{}, generation={}, format={}, component={}",
@@ -2946,7 +2948,7 @@ static std::expected<std::tuple<entry_descriptor, sstring, sstring>, sstring> ma
     //   la-42-big-Data.db
     //   ka-42-big-Data.db
     //   me-3g8w_00qf_4pbog2i7h2c7am0uoe-big-Data.db
-    static boost::regex la_mx("(la|m[cdest])-([^-]+)-(\\w+)-(.*)");
+    static boost::regex la_mx("(la|m[cdestu])-([^-]+)-(\\w+)-(.*)");
     static boost::regex ka("(\\w+)-(\\w+)-ka-(\\d+)-(.*)");
 
     // Use non-greedy match so that a snapshot tag that ressembles a name-<uuid> wouldn't match
@@ -3042,7 +3044,7 @@ sstable_format_types format_from_string(std::string_view s) {
 }
 
 bool has_summary_and_index(sstable_version_types v) {
-    return v != sstable_version_types::ms && v != sstable_version_types::mt;
+    return v != sstable_version_types::ms && v != sstable_version_types::mt && v != sstable_version_types::mu;
 }
 
 bool uses_legacy_dk_order(sstable_version_types v) {
