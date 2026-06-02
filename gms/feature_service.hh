222ac8fda7cf8015d499ee6b19bc1ede4165ab69
Michał Chojnowski <michal.chojnowski@scylladb.com>
2026-06-02 15:37:42 +0200

    db/config: expose sstable format version "mu"
    Allow selecting the new format version via config options
    and make it recognizable to sstables_manager.
    As usual, writing of the new format is gated by a new corresponding cluster feature.

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -170,6 +170,7 @@ public:
     gms::feature view_building_coordinator { *this, "VIEW_BUILDING_COORDINATOR"sv };
     gms::feature ms_sstable { *this, "MS_SSTABLE_FORMAT"sv };
     gms::feature mt_sstable { *this, "MT_SSTABLE_FORMAT"sv };
+    gms::feature mu_sstable { *this, "MU_SSTABLE_FORMAT"sv };
     gms::feature rack_list_rf { *this, "RACK_LIST_RF"sv };
     gms::feature driver_service_level { *this, "DRIVER_SERVICE_LEVEL"sv };
     gms::feature strongly_consistent_tables { *this, "STRONGLY_CONSISTENT_TABLES"sv };
