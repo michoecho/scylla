7d001df72dd46e83b5c38f39af75b54b552ec2ff
Michał Chojnowski <michocho98@gmail.com>
2026-06-28 19:25:02 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Template for C++ projects";
+
+  inputs = {
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs-stable, nixpkgs-unstable }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs-stable.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs-stable {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
+          };
+          llvmPkgs = pkgs.llvmPackages;
+        in
+        {
+          default = pkgs.mkShell.override { stdenv = pkgs.overrideCC pkgs.stdenv (pkgs.ccacheWrapper.override { cc = llvmPkgs.clang; }); } {
+            packages = with pkgs; [
+              cmake
+              ninja
+              llvmPkgs.clang-tools
+              llvmPkgs.llvm
+              gdb
+              boost.dev
+              boost
+              zstd
+              lz4
+            ];
+
+            hardeningDisable = [ "all" ];
+          };
+        });
+    };
+}
