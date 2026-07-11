60398b490f311aacd9493a8dda9d5aaf61806d02
Michał Chojnowski <michal.chojnowski@scylladb.com>
2026-07-11 19:30:10 +0200

    sstables: make reader_position_tracker a wrapper over sstable_position
    First step of teaching sstable readers to speak sstable_position.
    
    There's no behavior change in this commit.
    The sstable_position field isn't used yet.
    After the commit, the `offset` field contains what `position`
    used to contain before the commit, and readers rely on `offset`
    rather than `position` for now.
    This will change soon.

--- sstables/progress_monitor.hh
+++ sstables/progress_monitor.hh
@@ -12,6 +12,7 @@
 
 #include <seastar/core/shared_ptr.hh>
 #include "shared_sstable.hh"
+#include "sstables/sstable_position.hh"
 
 namespace sstables {
 
@@ -29,8 +30,8 @@ public:
 write_monitor& default_write_monitor();
 
 struct reader_position_tracker {
-    uint64_t position = 0;
-    uint64_t total_read_size = 0;
+    sstable_position position;
+    int64_t offset = 0;
 };
 
 class read_monitor {
